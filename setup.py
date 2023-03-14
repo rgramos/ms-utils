818a0bb777b9bd85fdca4444bc1b7f75bf3afdcd
Rene Gonzalez <rene.gonzalez@sistemas.valcoba.com>
2023-03-14 16:27:58 +0100

    authors

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ setup(
     include_package_data=True,
     version='0.0.20',
     description='General functions for the implementation of microservices.',
-    author='teisoftllc',
-    author_email="logistics@teisoftllc.com",
+    authors=[
+        {"name": "Alejandro A. Serrano Correa", "email": "alejandroasc93@gmail.com"},
+        {"name": "Rene Gonzalez Ramos", "email": "rgramos9310@gmail.com"}
+    ],
     license="GPLv3",
-    url="https://github.com/DynamicMedicalPlan/dmp-utils.git",
+    url="https://github.com/rgramos/ms-utils.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
