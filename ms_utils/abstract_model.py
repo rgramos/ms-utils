7ca505f4321c20ded4ef553c1c46bf480645447a
Rene Gonzalez <rene.gonzalez@sistemas.valcoba.com>
2023-03-17 13:23:39 +0100

    version 1.0.1 add id to base model

--- ms_utils/abstract_model.py
+++ ms_utils/abstract_model.py
@@ -3,12 +3,13 @@ Abstract Base Model
 """
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, DateTime, Integer
 
 
 class BaseModel:
     """
     Datetime created_at and updated_at
     """
+    id = Column(Integer, primary_key=True, autoincrement=True)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
