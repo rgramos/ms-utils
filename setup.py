a9df6c79f266505100a616623459a29ee8e08c10
Alejandro Serrano <alejandro.serrano@sistemas.vacolba.com>
2023-06-29 13:06:38 +0200

    update flask command to create deployment files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     package_data={'ms_utils': ['devops/*', 'devops/deploy/*']},
     include_package_data=True,
-    version='1.4.5',
+    version='1.4.6',
     description='General functions for the implementation of microservices.',
     authors=[
         {"name": "Alejandro A. Serrano Correa", "email": "alejandroasc93@gmail.com"},
