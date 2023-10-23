0a424db260f8d980156c7ff1720d7a8a4a320d3a
Alejandro Serrano <alejandro.serrano@sistemas.vacolba.com>
2023-10-23 11:18:28 +0200

    add try and except in schema_utils

--- ms_utils/schema_utils.py
+++ ms_utils/schema_utils.py
@@ -1,13 +1,20 @@
 from flask import current_app
 import requests
 
+
 def get_file(obj, f_key):
+    """
+    Get file from ms data file
+    """
     obj_file = getattr(obj, f_key)
-    if obj_file is None:
-        return None
-    url = f"{current_app.config.get('DATA_FILE_MS_URL')}/{current_app.config.get('DATA_FILE_MS_API')}/{obj_file}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        response = response.json()
-        return f"{current_app.config.get('DATA_FILE_MS_URL')}{response['data']['path']}"
+    try:
+        if obj_file is None:
+            return None
+        url = f"{current_app.config.get('DATA_FILE_MS_URL')}/{current_app.config.get('DATA_FILE_MS_API')}/{obj_file}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            response = response.json()
+            return f"{current_app.config.get('DATA_FILE_MS_URL')}{response['data']['path']}"
+    except Exception:
+        pass
     return None
