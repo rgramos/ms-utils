a1549916cb11476ceb0bb2e266913cad2b9e62ac
reneg <rene.gonzalez@sistemas.vacolba.com>
2023-12-04 13:29:44 +0100

    add dynamic_request and request_patch and add verify_false to all request utils method

--- ms_utils/requests_utils.py
+++ ms_utils/requests_utils.py
@@ -22,7 +22,7 @@ def request_get(url, params=None, headers=None):
     """
     if params is None:
         params = {}
-    return requests.get(url, params=params, headers=prepare_header(headers))
+    return requests.get(url, params=params, headers=prepare_header(headers), verify=False)
 
 
 def request_post(url, data=None, headers=None):
@@ -31,4 +31,33 @@ def request_post(url, data=None, headers=None):
     """
     if data is None:
         data = {}
-    return requests.post(url, json=data, headers=prepare_header(headers))
+    return requests.post(url, json=data, headers=prepare_header(headers), verify=False)
+
+
+def request_patch(url, data=None, headers=None):
+    """
+    Add authentication when sending "patch" requests
+    """
+    if data is None:
+        data = {}
+    return requests.patch(url, json=data, headers=prepare_header(headers), verify=False)
+
+
+def request_files(method, url, data=None, headers=None):
+    """
+    Add authentication when sending "post or patch" requests with files
+    """
+    if data is None:
+        data = {}
+    return requests.request(method, url, files=data, verify=False, headers=prepare_header(headers))
+
+
+def dynamic_request(method, url, data=None, headers=None, form_data=False):
+    """
+    Method to do dynamic requests
+    """
+    if data is None:
+        data = {}
+    if form_data:
+        return requests.request(method, url, files=data, verify=False, headers=prepare_header(headers))
+    return requests.request(method, url, json=data, verify=False, headers=prepare_header(headers))
