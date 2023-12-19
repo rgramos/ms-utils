bc57866bf6c01fff0fc062a8066a04875154bd03
Alejandro Serrano <alejandro.serrano@sistemas.vacolba.com>
2023-12-19 14:20:31 +0100

    update request verify

--- ms_utils/requests_utils.py
+++ ms_utils/requests_utils.py
@@ -1,5 +1,5 @@
 import requests
-from flask import request
+from flask import request, current_app
 
 
 def prepare_header(headers):
@@ -22,7 +22,7 @@ def request_get(url, params=None, headers=None):
     """
     if params is None:
         params = {}
-    return requests.get(url, params=params, headers=prepare_header(headers), verify=False)
+    return requests.get(url, params=params, headers=prepare_header(headers), verify=get_verify())
 
 
 def request_post(url, data=None, headers=None):
@@ -31,7 +31,7 @@ def request_post(url, data=None, headers=None):
     """
     if data is None:
         data = {}
-    return requests.post(url, json=data, headers=prepare_header(headers), verify=False)
+    return requests.post(url, json=data, headers=prepare_header(headers), verify=get_verify())
 
 
 def request_patch(url, data=None, headers=None):
@@ -40,7 +40,7 @@ def request_patch(url, data=None, headers=None):
     """
     if data is None:
         data = {}
-    return requests.patch(url, json=data, headers=prepare_header(headers), verify=False)
+    return requests.patch(url, json=data, headers=prepare_header(headers), verify=get_verify())
 
 
 def request_files(method, url, data=None, headers=None):
@@ -49,7 +49,7 @@ def request_files(method, url, data=None, headers=None):
     """
     if data is None:
         data = {}
-    return requests.request(method, url, files=data, verify=False, headers=prepare_header(headers))
+    return requests.request(method, url, files=data, verify=get_verify(), headers=prepare_header(headers))
 
 
 def dynamic_request(method, url, data=None, headers=None, form_data=False):
@@ -59,5 +59,11 @@ def dynamic_request(method, url, data=None, headers=None, form_data=False):
     if data is None:
         data = {}
     if form_data:
-        return requests.request(method, url, files=data, verify=False, headers=prepare_header(headers))
-    return requests.request(method, url, json=data, verify=False, headers=prepare_header(headers))
+        return requests.request(method, url, files=data, verify=get_verify(), headers=prepare_header(headers))
+    return requests.request(method, url, json=data, verify=get_verify(), headers=prepare_header(headers))
+
+
+def get_verify():
+    if 'REQUEST_VERIFY' in current_app.config.keys():
+        return current_app.config['REQUEST_VERIFY']
+    return False
