9a1d78193abd858f17e2c3ad57f892102a64b701
reneg <rene.gonzalez@sistemas.vacolba.com>
2024-01-09 10:11:17 +0100

    change version to 2.0.15

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     package_data={'ms_utils': ['devops/*', 'devops/deploy/*']},
     include_package_data=True,
-    version='2.0.14',
+    version='2.0.15',
     description='General functions for the implementation of microservices.',
     authors=[
         {"name": "Alejandro A. Serrano Correa", "email": "alejandroasc93@gmail.com"},
