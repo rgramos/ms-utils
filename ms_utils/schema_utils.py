735075b9493aeeb7c092263f42f0183ceb463b28
reneg <rene.gonzalez@sistemas.vacolba.com>
2024-01-15 12:31:12 +0100

    request get to get?file in schema utils

--- ms_utils/schema_utils.py
+++ ms_utils/schema_utils.py
@@ -1,5 +1,5 @@
 from flask import current_app
-import requests
+from .requests_utils import request_get
 
 
 def get_file(obj, f_key):
@@ -11,7 +11,7 @@ def get_file(obj, f_key):
         if obj_file is None:
             return None
         url = f"{current_app.config.get('DATA_FILE_MS_URL')}/{current_app.config.get('DATA_FILE_MS_API')}/{obj_file}"
-        response = requests.get(url)
+        response = request_get(url)
         if response.status_code == 200:
             response = response.json()
             return f"{current_app.config.get('DATA_FILE_MS_URL')}{response['data']['path']}"
